650e0bdc1c6e44400000cdee18aa451aa1d8ad4c
Chris R <Tratcher@outlook.com>
2019-01-24 09:56:38 -0800

    Move linux export

--- eng/scripts/install-nginx-linux.sh
+++ eng/scripts/install-nginx-linux.sh
@@ -4,4 +4,3 @@ curl -sSL http://nginx.org/download/nginx-1.14.2.tar.gz | tar zxfv - -C /tmp && 
 ./configure --prefix=$HOME/nginxinstall --with-http_ssl_module
 make
 make install
-export PATH="$PATH:$HOME/nginxinstall/sbin/"
