5580d3fc29b1153e32313e250777640f7487285f
Chris R <Tratcher@outlook.com>
2019-01-24 12:01:12 -0800

    Move export?

--- eng/scripts/install-nginx-mac.sh
+++ eng/scripts/install-nginx-mac.sh
@@ -4,4 +4,3 @@ brew update
 brew install openssl nginx
 ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
 ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
-export PATH="$PATH:$HOME/nginxinstall/sbin/"
