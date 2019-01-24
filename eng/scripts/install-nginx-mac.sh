853d9a6d565d70fdd84410e39e0f5d8c6be96572
Chris R <Tratcher@outlook.com>
2019-01-24 14:00:03 -0800

    Trim mac install

--- eng/scripts/install-nginx-mac.sh
+++ eng/scripts/install-nginx-mac.sh
@@ -2,5 +2,3 @@
 
 brew update
 brew install openssl nginx
-ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
-ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
